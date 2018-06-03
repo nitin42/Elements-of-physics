ad63786ca651023d42f21e5b7b0c39bdb0e23f04
Nitin <tulswani19@gmail.com>
2018-06-03 22:53:17 +0530

    prettir 🎉

--- src/index.js
+++ src/index.js
@@ -5,56 +5,67 @@ const p5 = require('p5')
 import { DrawBalls, p5Renderer } from './utils'
 
 const getRestProps = props => {
-	const { balls, maxSpeed, color, stroke, width, height, ballSize, measures, extras, ...rest } = props
+  const {
+    balls,
+    maxSpeed,
+    color,
+    stroke,
+    width,
+    height,
+    ballSize,
+    measures,
+    extras,
+    ...rest
+  } = props
 
-	return rest
+  return rest
 }
 
 export class Acceleration extends React.Component {
-	componentDidMount() {
-		this.getCanvas()
-	}
+  componentDidMount() {
+    this.getCanvas()
+  }
 
-	// Invoke the p5 renderer
-	renderer = p => p5Renderer(p, this.drawStuff, this.props)
+  // Invoke the p5 renderer
+  renderer = p => p5Renderer(p, this.drawStuff, this.props)
 
-	// Draw the balls on canvas
-	drawStuff = (p, dispatch, props) => {
-		let balls = new Array(props.balls || 20)
+  // Draw the balls on canvas
+  drawStuff = (p, dispatch, props) => {
+    let balls = new Array(props.balls || 20)
 
-		const setup = () => {
-			p.createCanvas(props.width, props.height)
+    const setup = () => {
+      p.createCanvas(props.width, props.height)
 
-			for (let i = 0; i < balls.length; i++) {
-				balls[i] = new DrawBalls(p, props)
-			}
-		}
+      for (let i = 0; i < balls.length; i++) {
+        balls[i] = new DrawBalls(p, props)
+      }
+    }
 
-		const draw = () => {
-			p.background(props.background)
+    const draw = () => {
+      p.background(props.background)
 
-			for (let i = 0; i < balls.length; i++) {
-				balls[i].updatePosition()
-				balls[i].hasCrossedEdge()
-				balls[i].displayBalls()
-			}
-		}
+      for (let i = 0; i < balls.length; i++) {
+        balls[i].updatePosition()
+        balls[i].hasCrossedEdge()
+        balls[i].displayBalls()
+      }
+    }
 
-		// Dispatch all the processing core functions
-		dispatch([setup, draw])
-	}
+    // Dispatch all the processing core functions
+    dispatch([setup, draw])
+  }
 
-	getCanvas = () =>
-		new p5(p => {
-			this.renderer(p)
+  getCanvas = () =>
+    new p5(p => {
+      this.renderer(p)
 
-			// Do some extra stuff here with processing
-			this.props.extras && this.props.extras(p)
-		}, this.wrapper)
+      // Do some extra stuff here with processing
+      this.props.extras && this.props.extras(p)
+    }, this.wrapper)
 
-	render() {
-		const rest = getRestProps(this.props)
+  render() {
+    const rest = getRestProps(this.props)
 
-		return <div ref={wrapper => (this.wrapper = wrapper)} {...rest} />
-	}
+    return <div ref={wrapper => (this.wrapper = wrapper)} {...rest} />
+  }
 }
